d8ea65ce14e9477025f47c4a33b0cf8fccf024dc
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-13 23:11:14 +0000

    log sooner

--- app/controllers/image_uploads_controller.rb
+++ app/controllers/image_uploads_controller.rb
@@ -3,10 +3,11 @@ class ImageUploadsController < AdminController
 	before_action :set_s3_direct_post, only: [:create]
 
   def create
-    params[:aws_path].each do |a|
-    	Rails.logger.info('******** --> ' + a)
-      @image = Image.create!(:path => a)
-    end
+  	Rails.logger.info('******** --> ' + params)
+    # params[:aws_path].each do |a|
+    # 	Rails.logger.info('******** --> ' + a)
+    #   @image = Image.create!(:path => a)
+    # end
     redirect_to admin_index_path
   end
 end
