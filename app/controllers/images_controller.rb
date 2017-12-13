ab8e50c057fa9830b6e0b544c06651c6cad3fef3
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-13 23:50:40 +0000

    more logs

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,7 +4,9 @@ class ImagesController < ApplicationController
 		@image = current_user.next_image
 		render :empty and return if !@image
 
+		Rails.logger.info("IMAGE")
 		Rails.logger.info(@image)
+		Rails.logger.info(@image.path)
 
 		@questions = @image.unanswered_questions_for_user(@current_user)
 		@start_time = Time.now
