32368d27bc4b2c101d869e575a47dc3a2606626a
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-13 23:24:59 +0000

    try this

--- app/controllers/image_uploads_controller.rb
+++ app/controllers/image_uploads_controller.rb
@@ -5,6 +5,13 @@ class ImageUploadsController < AdminController
   def create
   	Rails.logger.info("******** --> ")
   	Rails.logger.info(params)
+
+  	params[:images]['avatar'].each do |avatar|
+  		if avatar.is_a? String
+    		Rails.logger.info('AVATAR ******** --> ' + avatar)
+      		@image = Image.create!(:path => avatar)
+      	end
+    end
     # params[:aws_path].each do |a|
     # 	Rails.logger.info('******** --> ' + a)
     #   @image = Image.create!(:path => a)
