3d49984a4b83257bb50579ec86daed9a81ed8ef9
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-13 22:17:34 +0000

    remove raise

--- app/controllers/image_uploads_controller.rb
+++ app/controllers/image_uploads_controller.rb
@@ -3,7 +3,6 @@ class ImageUploadsController < AdminController
 	before_action :set_s3_direct_post, only: [:create]
 
   def create
-  	raise
     params[:images]['file'].each do |a|
       @image = Image.create!(:path => a)
     end
