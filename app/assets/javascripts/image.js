93f60df1b5a40135ffc32edbbbaed4b755f023ba
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-13 22:33:44 +0000

    comment out and see

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -39,48 +39,51 @@ $(document).ready(function(){
     var progressBar  = $("<div class='bar'></div>");
     var barContainer = $("<div class='progress'></div>").append(progressBar);
     fileInput.after(barContainer);
-    fileInput.fileupload({
-      fileInput:       fileInput,
-      url:             form.data('url'),
-      type:            'POST',
-      autoUpload:       true,
-      formData:         form.data('form-data'),
-      paramName:        'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
-      dataType:         'XML',  // S3 returns XML if success_action_status is set to 201
-      replaceFileInput: false,
-      progressall: function (e, data) {
-        var progress = parseInt(data.loaded / data.total * 100, 10);
-        progressBar.css('width', progress + '%')
-      },
-      start: function (e) {
-        submitButton.prop('disabled', true);
 
-        progressBar.
-          css('background', 'green').
-          css('display', 'block').
-          css('width', '0%').
-          text("Loading...");
-      },
-      done: function(e, data) {
-        submitButton.prop('disabled', false);
-        progressBar.text("Uploading done");
+    console.log('file input');
+    console.log(fileInput);
+    // fileInput.fileupload({
+    //   fileInput:       fileInput,
+    //   url:             form.data('url'),
+    //   type:            'POST',
+    //   autoUpload:       true,
+    //   formData:         form.data('form-data'),
+    //   paramName:        'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
+    //   dataType:         'XML',  // S3 returns XML if success_action_status is set to 201
+    //   replaceFileInput: false,
+    //   progressall: function (e, data) {
+    //     var progress = parseInt(data.loaded / data.total * 100, 10);
+    //     progressBar.css('width', progress + '%')
+    //   },
+    //   start: function (e) {
+    //     submitButton.prop('disabled', true);
 
-        // extract key and generate URL from response
-        var key   = $(data.jqXHR.responseXML).find("Key").text();
-        var url   = '//' + form.data('host') + '/' + key;
+    //     progressBar.
+    //       css('background', 'green').
+    //       css('display', 'block').
+    //       css('width', '0%').
+    //       text("Loading...");
+    //   },
+    //   done: function(e, data) {
+    //     submitButton.prop('disabled', false);
+    //     progressBar.text("Uploading done");
 
-        // create hidden field
-        var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: url })
-        form.append(input);
-      },
-      fail: function(e, data) {
-        submitButton.prop('disabled', false);
+    //     // extract key and generate URL from response
+    //     var key   = $(data.jqXHR.responseXML).find("Key").text();
+    //     var url   = '//' + form.data('host') + '/' + key;
 
-        progressBar.
-          css("background", "red").
-          text("Failed");
-      }
-    });
+    //     // create hidden field
+    //     var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: url })
+    //     form.append(input);
+    //   },
+    //   fail: function(e, data) {
+    //     submitButton.prop('disabled', false);
+
+    //     progressBar.
+    //       css("background", "red").
+    //       text("Failed");
+    //   }
+    // });
   });
 });
 
