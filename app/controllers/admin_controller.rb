25a180108e999bb25a9824e0c2c94b9c0d25f8dc
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-14 00:18:30 +0000

    what if csv is easy

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -15,6 +15,24 @@ class AdminController < ApplicationController
 		end
 	end
 
+	def download_data
+		@csv_string = CSV.generate do |csv|
+			csv << ['image', 'question', 'user', 'response', 'start_time', 'submit_time']
+
+			Answer.all.sort(:submit_time).each do |answer|
+				image = answer.image.path.split('/').last
+				question = answer.question.text
+				user = answer.user.email
+				response = answer.response
+				start_time = answer.start_time
+				submit_time = answer.submit_time
+				csv << [image, question, user, response, start_time, submit_time]
+			end
+		end
+
+		send_file(@csv_string)
+	end
+
 	def set_s3_direct_post
    		@s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
   	end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :image_uploads
   	devise_for :users, controllers: { registrations: "registrations"}
 	root to: "images#show"
+	get "admin/download_data"
 	resources :answers, only: [:create]
 	resources :admin, only: [:index]
 	resources :image_uploads
